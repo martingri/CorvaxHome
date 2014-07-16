e8bb0ab8f11b4eb22625e45d78fec8bad6c7b4ae
Martin Grimeland <martin.grimeland@gmail.com>
2014-07-16 09:41:33 +0200

    Setting locale nb_NO as default in Module.php

--- module/GrimelandCoding/Module.php
+++ module/GrimelandCoding/Module.php
@@ -12,6 +12,12 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+	$translator = $e->getApplication()->getServiceManager()->get('translator');
+	$http_accept_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        $translator
+          ->setLocale(\Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+          ->setFallbackLocale('nb_NO');
+
     }
 
     public function getConfig()
