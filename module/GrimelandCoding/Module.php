5c308a649d00e0506ad9ad1740d63ca5ce748976
Martin Grimeland <martin.grimeland@gmail.com>
2014-07-24 16:30:25 +0100

    Setup the Module to use a part of the url parameter to determine language, falling back on accept language or a default

--- module/GrimelandCoding/Module.php
+++ module/GrimelandCoding/Module.php
@@ -9,15 +9,42 @@ class Module
 {
     public function onBootstrap(MvcEvent $e)
     {
-        $eventManager        = $e->getApplication()->getEventManager();
+	$eventManagerWithCallback = $this->setRouteCallback($e);
         $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-	$translator = $e->getApplication()->getServiceManager()->get('translator');
-	$http_accept_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-        $translator
-          ->setLocale(\Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']))
-          ->setFallbackLocale('nb_NO');
+        $moduleRouteListener->attach($eventManagerWithCallback);
+    }
 
+    private function setRouteCallback($e) 
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $routeCallback = function ($e) {
+            $availableLanguages = array ('no' => 'nb_NO', 
+	    			  	 'en' => 'en_US' );
+            $defaultLanguage = 'en';
+            $language = "";
+            $fromRoute = false;
+            //see if language could be find in url
+            if ($e->getRouteMatch()->getParam('lang')) {
+                $language = $e->getRouteMatch()->getParam('lang');
+                $fromRoute = true;
+                //or use language from http accept
+            } else {
+                $headers = $e->getApplication()->getRequest()->getHeaders();
+                if ($headers->has('Accept-Language')) {
+                    $headerLocale = $headers->get('Accept-Language')->getPrioritized();
+                    $language = substr($headerLocale[0]->getLanguage(), 0,2);
+                }
+            }
+            if(!$availableLanguages[$language]) {
+                $language = $defaultLanguage;
+            }
+	    $locale = $availableLanguages[$language];
+	    $translator = $e->getApplication()->getServiceManager()->get('translator');
+	    $translator->setLocale($locale);
+        };
+
+        $eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_ROUTE, $routeCallback);
+	return $eventManager;
     }
 
     public function getConfig()
